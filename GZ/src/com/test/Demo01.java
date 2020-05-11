ee86913a3cdc90b1c5e1f04a1097b8cbe99b5b3f
Exception-Hu <54347983+Exception-Hu@users.noreply.github.com>
2020-05-11 13:06:39 +0800

    添加delete方法

--- GZ/src/com/test/Demo01.java
+++ GZ/src/com/test/Demo01.java
@@ -3,16 +3,5 @@ package com.test;
 public interface Demo01 {
 	void addUser();
 	
-	void aaa();
-	
-	void bbb();
-        
-    void ccc();
-    
-    //
-    void ddd(); 
-    
-    void test_brand();
-    
-    void master();
+	void delete();
 }
